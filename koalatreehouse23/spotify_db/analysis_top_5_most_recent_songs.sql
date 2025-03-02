0e8ce56c237886980f1272b4eb2954ac04d58010
Ha Dinh <32108841+hadinh1306@users.noreply.github.com>
2025-03-01 22:47:50 -0800

    Example how to set up database and query (#6)
    * Example how to set up database and query
    
    * Update sql file

---
+++ koalatreehouse23/spotify_db/analysis_top_5_most_recent_songs.sql
@@ -0,0 +1,6 @@
+SELECT 
+    name,
+    album_release_date
+FROM playlist_tracks
+ORDER BY album_release_date DESC
+LIMIT 5;
---
+++ koalatreehouse23/spotify_db/schema/create_playlist_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS playlist_tracks (
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR,
+    album_release_date DATETIME
+);
